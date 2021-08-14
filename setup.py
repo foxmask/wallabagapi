3b72339ce829454d95b0dfe08cec1c22b2187185
FoxMaSk <foxmask@protonmail.com>
2021-08-14 21:52:48 +0200

    upd setup and CI

--- setup.py
+++ setup.py
@@ -5,11 +5,13 @@ from setuptools import setup, find_packages
 
 import wallabagapi
 
+version = wallabagapi.__version__
+
 setup(
 
     name='wallabagapi',
 
-    version=wallabagapi.__version__,
+    version=version,
 
     packages=find_packages(),
 
@@ -19,16 +21,17 @@ setup(
     description="Wallabag API to add every pages you want to your Wallabag account",
     long_description=open('README.rst').read(),
 
-    url='https://gitlab.com/foxmask/wallabagapi',
+    url='https://gitlab.com/foxmask/wallabag_api',
+    download_url='https://gitlab.com/foxmask/wallabag_api/-/archive/' + version + 'wallabagapi-' + version + '.tar.gz',
 
     classifiers=[
-        "Development Status :: 5 - Production/Stable"
-        "Intended Audience :: Developers"
-        "License :: Public Domain"
-        "Operating System :: OS Independent"
-        "Programming Language :: Python"
-        "Programming Language :: Python :: 3.8"
-        "Topic :: Internet"
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: Public Domain",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Internet",
         "Topic :: Communications"
     ],
 
